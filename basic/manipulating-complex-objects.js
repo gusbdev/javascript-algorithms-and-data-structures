8aebdc873e94fdd8758e17d52cd4ba459b40d733
Gus <gusbdev@gmail.com>
2021-08-04 12:29:04 -0300

    Lesson 90

---
+++ basic/manipulating-complex-objects.js
@@ -0,0 +1,22 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Metallica",
+    "title": "The Black Album",
+    "release_year": 1991,
+    "formats": [
+      "CD",
+      "LP"
+    ]
+  }
+];
